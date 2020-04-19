f16a44ee3a0b2462f528ec1b6df0b8009fc43093
Siddalingaprasad Raju <sid.rsp@gmail.com>
2020-04-19 20:45:25 +0530

    added hystrix fallbacks individually, but this will throw error

--- src/main/java/com/sidrsp/moviecatalogservice/resources/MovieCatalogResource.java
+++ src/main/java/com/sidrsp/moviecatalogservice/resources/MovieCatalogResource.java
@@ -3,6 +3,7 @@ package com.sidrsp.moviecatalogservice.resources;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.sidrsp.moviecatalogservice.resources.models.CatalogItem;
 import com.sidrsp.moviecatalogservice.resources.models.Movie;
+import com.sidrsp.moviecatalogservice.resources.models.Rating;
 import com.sidrsp.moviecatalogservice.resources.models.UserRating;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -35,29 +36,54 @@ public class MovieCatalogResource {
     private DiscoveryClient discoveryClient;
     */
     @RequestMapping("/{userId}")
-    @HystrixCommand(fallbackMethod = "getFallbackCatalog")
     public List<CatalogItem> getCatalog(@PathVariable String userId) {
 
-        /* using RestTemplate */
-        UserRating userRating = restTemplate.getForObject(
-                "http://ratings-data-service/ratingsdata/users/" + userId, UserRating.class
-        );
+        UserRating userRating = getUserRating(userId);
 
         /* using WebClient */
         return userRating.getUserRating().stream().map(rating -> {
-            Movie movie = webClientbuilder.build()
-                    .get()
-                    .uri("http://movie-info-service/movies/" + rating.getMovieId())
-                    .retrieve()
-                    .bodyToMono(Movie.class)
-                    .block();
-
+            Movie movie = getMovieInfo(rating);
             return new CatalogItem(movie.getName(), "desc-test", rating.getRating());
         }).collect(Collectors.toList());
     }
 
-    public List<CatalogItem> getFallbackCatalog(@PathVariable String userId) {
-        return Arrays.asList(new CatalogItem("fallback name", "fallback desc", 4));
+    /*
+    * Note: When a fallback method is defined in the same class, hystrix has no way to get
+    * the instance to call the fallback methods as it always holds on to instance of proxy class,
+    * but if we define the fallback methods in an external class as service or beans and then
+    * autowire, hystrix gets the proxy instance to call fallbacks
+    *
+    * */
+    @HystrixCommand(fallbackMethod = "getFallbackUserRating")
+    private UserRating getUserRating(String userId) {
+        /* using RestTemplate */
+        return restTemplate.getForObject(
+                "http://ratings-data-service/ratingsdata/users/" + userId, UserRating.class
+        );
+    }
+
+    private UserRating getFallbackUserRating(String userId) {
+        UserRating userRating = new UserRating();
+        userRating.setUserRating(
+                Arrays.asList(
+                        new Rating("fallback movieId", 0)
+                )
+        );
+        return userRating;
+    }
+
+    @HystrixCommand(fallbackMethod = "getFallbackMovieInfo")
+    private Movie getMovieInfo(Rating rating) {
+        return webClientbuilder.build()
+                .get()
+                .uri("http://movie-info-service/movies/" + rating.getMovieId())
+                .retrieve()
+                .bodyToMono(Movie.class)
+                .block();
+    }
+
+    private Movie getFallbackMovieInfo(Rating rating) {
+        return new Movie(rating.getMovieId(), "fallback movie name");
     }
 
 }
