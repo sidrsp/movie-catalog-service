c2a07a1582e454ea43ba848e052bb7f3af7d0d4a
Siddalingaprasad Raju <sid.rsp@gmail.com>
2020-04-25 21:14:51 +0530

    using ConfigurationProperties to load the app properties

--- src/main/java/com/sidrsp/moviecatalogservice/resources/GreetingController.java
+++ src/main/java/com/sidrsp/moviecatalogservice/resources/GreetingController.java
@@ -1,5 +1,6 @@
 package com.sidrsp.moviecatalogservice.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.Map;
 
+import com.sidrsp.moviecatalogservice.configs.DbSettings;
+
 @RestController
 @RequestMapping("/greeting")
 public class GreetingController {
+
+    @Autowired
+    private DbSettings dbSettings;
 
     @Value("${greeting.message: default Hi}")
     private String greetingMsg;
@@ -20,13 +26,17 @@ public class GreetingController {
     @Value("${my.list.values}")
     private List<String> listValues;
 
-
     @Value("#{${dbValues}}")
     private Map<String, String> dbValueMap;
 
     @RequestMapping("/sayhello")
-    public String greeting(){
+    public String greeting() {
         return greetingMsg + staticMessage + listValues + dbValueMap.get("username");
+    }
+
+    @RequestMapping("/withConfigProperties")
+    public String withConfigProperties() {
+        return greetingMsg + dbSettings.getConnection() + dbSettings.getHost() + dbSettings.getPort();
     }
 
 }
